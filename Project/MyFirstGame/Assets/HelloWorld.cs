3e00cd86d463efd31e2c8bae3e9aa19aadda4bb2
AlecCross <cross2472@gmail.com>
2021-02-08 23:39:23 +0200

    lw1-unity

---
+++ Project/MyFirstGame/Assets/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Функции котороые выполняются один раз
+        print("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //метод выполняется в один кадр
+        //Юнити работает в 30ти кадрах в сек
+    }
+}
